aebca9ab949bbec184bc303aaa65774a51f57b8a
Panagiotis Fotiou <panos9409@gmail.com>
2020-10-05 00:47:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ from connect_db import *
 conn, cursor = connect_to_db()
 
 
-# empty_table('events')
-# empty_table('production')
-# empty_table('venue')
-# empty_table('organizer')
+#empty_table('events')
+#empty_table('contributions')
+#empty_table('production')
+#empty_table('venue')
+#empty_table('organizer')
 #empty_table('persons')
-# begin_productions_scraping()
+#empty_table('roles')
+#begin_productions_scraping()
 #begin_actors_scraping()
 
 #scrap_events('https://www.viva.gr/tickets/theater/pollaploi-choroi/den-akouw-de-vlepw-de-milaw')
-#scrap_by_production('https://www.viva.gr/tickets/theater/anoigei-avlaia/oloi-mazi-mporoume')
+#scrap_by_production('https://www.viva.gr/tickets/theatre/pallas/trito-stefani')
 
-#scrap_persons('https://www.viva.gr/tickets/theater/periodeia/perimenontas-ton-gkonto')
-scrap_persons('https://www.viva.gr/tickets/theater/periodeia/art-kalokairini-periodeia')
+scrap_persons('https://www.viva.gr/tickets/theatre/pallas/trito-stefani')
+#scrap_persons('https://www.viva.gr/tickets/theater/periodeia/art-kalokairini-periodeia')
 
 conn.close()
